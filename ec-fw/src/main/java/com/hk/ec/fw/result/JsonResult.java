9b2eb38ce61c9ed4476128a9246429dce4320f68
tongchao <tongchao@goldpalm.com.cn>
2015-07-01 17:11:41 +0800

    JsonResult
    把jsonresult和serviceresoult合并一起使用

--- ec-fw/src/main/java/com/hk/ec/fw/result/JsonResult.java
+++ ec-fw/src/main/java/com/hk/ec/fw/result/JsonResult.java
@@ -1,104 +1,132 @@
 package com.hk.ec.fw.result;
 
-
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.hk.ec.fw.spring.ext.PropertiesUtils;
 
 /**
- * ajax json 返回值包装对象
- * @author James
- *
+ * 返回值包装对象
+ * @param <T>
+ * 
  */
-public class JsonResult implements Serializable {
-	
-	/**
-	 * 序列化ID
-	 */
-	private static final long serialVersionUID = -7073856910135669765L;
-	/**
-	 * 操作结果（成功或失败）
-	 */
-	boolean isOk = true;
-	/**
-	 * 操作状态码
-	 */
-	String stateCode = StateCode.SUCCESS_CODE;
-	/**
-	 * 操作提示信息
-	 */
-	String message;
-	/**
-	 * 备注
-	 */
-	String comment;
-	/**
-	 * 数据
-	 */
-	Object data;
+public class JsonResult<T> implements Serializable {
 
-	public boolean isOk() {
-		return isOk;
-	}
+	private static final long serialVersionUID = -6652076509848001811L;
 
-	public String getStateCode() {
-		return stateCode;
-	}
-
-	public void setStateCode(String stateCode) {
-		this.stateCode = stateCode;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
+	public JsonResult(){}
+	public JsonResult(boolean ok, String message){
+		this.ok = ok;
 		this.message = message;
 	}
 
+	public JsonResult(T data, boolean isOk, String message){
+		this.data = data;
+		this.ok = isOk;
+		this.message = message;
+	}
+	
+	private boolean ok = false;
+	
+	/**
+	 *  msg 状态0表示异常、1表示正确
+	 */
+	private int messageType = 0;
+	
+	/**
+	 * 备注
+	 */
+	private String comment;
+	/**
+	 * 消息code
+	 */
+	private String messageCode;
+	private String message;
+	/**
+	 * 数据(单类型对象)
+	 */
+	private T data;
+	/**
+	 * 数据Map（多类型对象）
+	 */
+	private Map<String, Object> dataMap = new HashMap<String, Object>();
+
+	public boolean isOk() {
+		return ok;
+	}
+	public void setOk(boolean ok) {
+		this.ok = ok;
+	}
+	
+	public int getMessageType(){
+		return this.messageType;
+	}
+	public String getMessageCode() {
+		return messageCode;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	/**
+	 * 根据msg第二位字母取得msg类型
+	 * @param messageCode
+	 */
+	public void setMessageCode(String messageCode) {
+		this.messageCode = messageCode;
+		try {
+			char state = messageCode.charAt(1);
+			switch (state) {
+			case 'I':
+				this.messageType = 1;
+				break;
+			case 'Q':
+				this.messageType = 1;
+				break;
+			case 'E':
+				this.messageType = 0;
+				break;
+				
+			default:
+				this.messageType = 0;
+				break;
+			}
+			
+			// 到properties里面取得消息
+			this.message = PropertiesUtils.getString(messageCode);
+			
+		} catch (Exception e) {
+			this.messageType = 0;
+		}
+	}
+	
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public Map<String, Object> getDataMap() {
+		return dataMap;
+	}
+	public void setDataMap(Map<String, Object> dataMap) {
+		this.dataMap = dataMap;
+	}
+	public void putData(String key, Object value) {
+		this.dataMap.put(key, value);
+	}
+	
 	public String getComment() {
 		return comment;
 	}
-
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
 
-	public Object getData() {
-		return data;
-	}
-
-	public void setData(Object data) {
-		this.data = data;
-	}
-
-	public void setOk(boolean isOk) {
-		this.isOk = isOk;
-	}
-	
-	public class StateCode {
-		
-		/**
-		 * 成功状态码
-		 */
-		public static final String SUCCESS_CODE ="100";
-		
-		/**
-		 * 错误状态码
-		 */
-		public static final String ERROR_CODE ="200";
-		
-		
-		/**
-		 * 参数错误状态码
-		 */
-		public static final String ERROR_PARAM_CODE ="201";
-		
-		/**
-		 * 无数据错误状态码
-		 */
-		public static final String ERROR_NODATA_CODE ="202";
-		
-		
-	}
-	
 }
--- ec-sample/ec-sample-ms/src/main/java/com/palmyou/v7data/ms/controller/IndexController.java
+++ ec-sample/ec-sample-ms/src/main/java/com/palmyou/v7data/ms/controller/IndexController.java
@@ -1,13 +1,22 @@
 package com.palmyou.v7data.ms.controller;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.fragment.IFragmentSpec;
+import org.thymeleaf.standard.fragment.StandardDOMSelectorFragmentSpec;
 
+import com.hk.ec.fw.result.JsonResult;
 import com.hk.ec.fw.web.permission.WebUser;
 import com.palmyou.v7data.ms.util.MsgUtil;
 
@@ -15,6 +24,24 @@ import com.palmyou.v7data.ms.util.MsgUtil;
 public class IndexController extends BaseController{
 	private static final Logger logger = Logger.getLogger(IndexController.class);
 
+	@Resource
+	TemplateEngine templateEngine;
+	
+	@RequestMapping("/thtemplate")
+	@ResponseBody
+    public JsonResult<String> getString(HttpServletRequest request, HttpServletResponse response) {
+		
+		WebContext context = new WebContext(request, response, request.getServletContext());
+		String templateName = "tpls/userTpls";
+		// 标准DOM选择器是th:fragment="content"
+		IFragmentSpec fragmentSpec = new StandardDOMSelectorFragmentSpec("content");
+		context.setVariable("name", "我是名字");
+		context.setVariable("name1", "我是名字1");
+		String html = templateEngine.process(templateName, context, fragmentSpec);
+		System.out.println(html);
+        return new JsonResult<String>(html,false,"xxx");
+    }
+	
 	@RequestMapping({"/home"})
 	public void home(HttpSession session){
 		
