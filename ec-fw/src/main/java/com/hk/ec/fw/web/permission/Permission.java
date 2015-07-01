d6c0da3e8c4d365297ad88332125daa0cf02e096
tongchao <tongchao@goldpalm.com.cn>
2015-07-01 09:36:09 +0800

    new ec-fw import
    把ec-fw独立出来，方便其它项目依赖使用

---
+++ ec-fw/src/main/java/com/hk/ec/fw/web/permission/Permission.java
@@ -0,0 +1,18 @@
+package com.hk.ec.fw.web.permission;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)  
+@Target(ElementType.METHOD)
+public @interface Permission {  
+  
+    /** 
+     * 功能ID，该功能ID，对应数据库中的功能ID
+     * @return 
+     */  
+    String value();  
+      
+}
